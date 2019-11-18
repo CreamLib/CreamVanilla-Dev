c416da51c0d248302f9ac9f9111cff741b7976df
Thomas Robbe <thomas.robbe62@gmail.com>
2019-11-18 17:52:30 +0100

    Select & Datalist page

--- assets/js/librairie.js
+++ assets/js/librairie.js
@@ -11,6 +11,16 @@ function inputRequired() {
     })
 }
 
+function outputUpdate() {
+    var slider = document.getElementById('slider1');
+    var output = document.getElementById('output');
+    output.innerHTML = slider.value;
+    document.addEventListener('change', function() {
+        output.innerHTML = slider.value;
+    })
+}
+
 document.addEventListener('DOMContentLoaded', function () {
     inputRequired();
+    outputUpdate();
 });
